47e9dfac92dbbb7305a157a6d8411d0269d63dda
Brandon Dias <brandon.dias@castgroup.com.br>
2026-03-27 16:55:15 -0300

    feat: add tests for message creation using Prisma

---
+++ api/src/tests/tests.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client.ts";
+
+const prisma = new PrismaClient();
+
+async function main() {
+
+   const message = await prisma.message.create({
+    data: {
+        authorName: "Markin Relâmpago",
+        content: "O tigre é o felino e na caça é implacável.",
+    },
+  }); 
+
+    console.log("Message created:", message);
+
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  });
